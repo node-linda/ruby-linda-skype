40001f63c343aec60cd95a1f99cf023a56639c32
Sho Hashimoto <hashimoto@shokai.org>
2014-01-24 01:40:31 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "foreman"
+gem "linda-socket.io-client"
+gem "skype"
+gem "ruby-dbus" if RUBY_PLATFORM =~ /linux/
---
+++ linda-skype.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'skype'
+require 'linda-socket.io-client'
+$stdout.sync = true
+
+ENV['LINDA_BASE'] ||= 'http://node-linda-base.herokuapp.com'
+ENV['LINDA_SPACE'] ||= 'test'
+ENV["CHAT_TOPIC"]  ||= 'linda'
+
+chat = Skype.chats.find{|c| c.topic =~ /#{ENV['CHAT_TOPIC']}/ }
+
+unless chat
+  STDERR.puts "chat not found"
+  exit 1
+end
+
+puts "chat id:#{chat.id}  topic:#{chat.topic}"
+
+linda = Linda::SocketIO::Client.connect ENV['LINDA_BASE']
+ts = linda.tuplespace(ENV['LINDA_SPACE'])
+
+linda.io.on :connect do
+  puts "connect!!"
+  puts "watching {type: 'skype', cmd: 'send'} in tuplespace #{ts.name}"
+  puts " => #{linda.url}/#{ts.name}?type=skype&cmd=send&value=hello"
+
+  ts.watch type: "skype", cmd: "send" do |err, tuple|
+    p tuple
+    next if tuple["data"].has_key? "response"
+    str = tuple["data"]["value"].to_s
+    next if str.empty?
+    msg = "(ninja) 《Linda》 #{str} 《#{ts.name}》"
+    chat.post msg
+    ts.write type: "skype", cmd: "send", value: str, response: "success"
+  end
+
+end
+
+linda.io.on :disconnect do
+  puts "socket.io disconnect"
+end
+
+loop do
+  sleep 1
+end
+
